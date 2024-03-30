6cf2abe7221ed777fa0f810ce6cba456226ca964
Nabeera Khan <nabeera600@gmail.com>
2024-03-30 05:08:29 +0500

    linking

---
+++ src/app/homepage/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+const Homepage = () => {
+    return (<div>
+     This is homepage
+     <br/>
+     <Link href = '/'>back</Link>
+    </div>)
+    
+}
+
+export default Homepage
---
+++ src/app/namepage/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function namepage (){
+  return (
+    <div>Nabeera khan
+    <br/>
+    <Link href = '/'>back</Link>
+    </div>
+  )
+}
